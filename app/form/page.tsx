1f814c5c684354e3082f5aa72bcaa7ec46f78678
Taro112233 <thanatouchth@gmail.com>
2025-12-18 23:11:36 +0700

    setup

--- app/form/page.tsx
+++
@@ -1,6 +0,0 @@
-// app/form/page.tsx
-import { AdultAssessmentFormComplete } from '@/components/forms/adult-assessment-form-complete';
-
-export default function AdultPage() {
-  return <AdultAssessmentFormComplete />;
-}
